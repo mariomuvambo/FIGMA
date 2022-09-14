0ae93f1738d4ed662cfc1fe5c9a21df024ddf88e
Mario Muvambo <mariomuvambo1@gmail.com>
2022-09-14 12:31:16 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { FilterComponent } from './filter/filter.component';
+import { CountryComponent } from './country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    FilterComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
