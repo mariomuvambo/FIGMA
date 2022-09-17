2cda144b7d3034aa96870e0246df5450fb0c9674
Mario Muvambo <mariomuvambo1@gmail.com>
2022-09-17 12:34:49 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { FilterComponent } from './filter/filter.component';
 import { CountryComponent } from './country/country.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     FilterComponent,
-    CountryComponent
+    CountryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
